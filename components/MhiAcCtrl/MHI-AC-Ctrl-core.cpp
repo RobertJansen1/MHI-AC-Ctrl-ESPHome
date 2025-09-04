6a4055d4d92cf1557dc1fd12abefcef88b06c261
Robert Jansen <r.jansen86@gmail.com>
2025-09-04 14:19:42 +0200

    error

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
@@ -282,10 +282,11 @@ int MHI_AC_Ctrl_Core::loop(uint max_time_ms) {
     // overwrite MISO_frame with received data when in read only mode
     if (read_only_mode_) {
       // ESP_LOGW("mhi_ac_ctrl_core", "In read-only mode, overwriting MISO_frame byte %d: 0x%02X with received byte: 0x%02X", byte_cnt, MISO_frame[byte_cnt], MISO_received_byte);
-      if (MISO_Fetched_frame[byte_cnt] != MISO_received_byte)
+      if (MISO_Fetched_frame[byte_cnt] != MISO_received_byte) {
         new_datapacket_received = true;
         MISO_Fetched_frame[byte_cnt] = MISO_received_byte;
-      MISO_Frame[byte_cnt] = MISO_received_byte;
+      }
+      MISO_frame[byte_cnt] = MISO_received_byte;
     }
   }
   // Debug output for MISO and MOSI frames
