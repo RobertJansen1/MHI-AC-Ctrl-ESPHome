59f43237a5ef9c6f77028f91c263aab61a3b1bea
Robert Jansen <r.jansen86@gmail.com>
2025-09-11 16:11:16 +0200

    longer timeout

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
@@ -333,14 +333,14 @@ int MHI_AC_Ctrl_Core::loop(uint max_time_ms) {
     for (uint8_t bit_cnt = 0; bit_cnt < 8; bit_cnt++) { // read and write 1 byte
   
       if (!read_only_mode_) {
-        delayMicroseconds(1);
+        delayMicroseconds(10);
         if ((MISO_frame[byte_cnt] & bit_mask) > 0)
           digitalWrite(MISO_PIN, 1);
         else
           digitalWrite(MISO_PIN, 0);
       }
       while (!digitalRead(SCK_PIN)) {} // wait for rising edge
-      delayMicroseconds(1);
+      delayMicroseconds(10);
       if (digitalRead(MOSI_PIN))
         MOSI_byte += bit_mask;
       
